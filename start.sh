099ef3a22fa4a46153582201962f6dcff37f1e2d
Lyss <shaitan.lyssm@gmail.com>
2021-07-27 01:53:28 +0200

    Add success / failure messages on start

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 sudo systemctl restart Bot_Base
-sudo systemctl status -l Bot_Base
+
+sub_state=$(systemctl show -p SubState Bot_Base --value)
+
+if [ "$sub_state" = "running" ]; then
+  echo "Bot successfully started !"
+else
+  echo "Failure... Use 'systemctl status Bot_Base' for more information."
+fi
