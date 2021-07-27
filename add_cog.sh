891d459c7d82433c942ef3969623d433e0b64091
Lyss <shaitan.lyssm@gmail.com>
2021-07-27 03:32:06 +0200

    Fix add_cog.sh

--- add_cog.sh
+++ add_cog.sh
@@ -7,19 +7,24 @@ repo="$tmp/$1"
 
 # checks repo name has been supplied
 if [ -z ${1+x} ]; then
-  echo error: repo_name unfilled
+  echo 'error: argument not supplied'
   echo
   echo 'usage : ./add_cog.sh <repo_name>'
   exit
 fi
 
-# downloads the repo from github.com
-git clone "https://github.com/UnionRolistes/$1.git" "$repo" # TODO add several cogs at once
+if [ ! -e "$repo" ] ; then
+  # downloads the repo from github.com
+  git clone "https://github.com/UnionRolistes/$1.git" "$repo" # TODO add several cogs at once
 
-# checks that the clone was successful
-if [ ! -e "$repo" ]; then
-  echo Failure
-  exit
+  # checks that the clone was successful
+  if [ ! -e "$repo" ]; then
+    echo Failure
+    exit
+  fi
+else
+  # updates the repo
+  git pull "$repo"
 fi
 
 cp -vr "$repo/src/." $cogs_folder
