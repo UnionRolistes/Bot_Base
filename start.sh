4f182f1024bdd40c083b065e20e43379c95f6d20
Lyss <shaitan.lyssm@gmail.com>
2021-07-27 02:02:48 +0200

    Fix success / failure msgs

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
+# start / restart the service
 sudo systemctl restart Bot_Base
 
+# wait a little to find out if the service was successfully started
+sleep 1
+
+# check SubState
 sub_state=$(systemctl show -p SubState Bot_Base --value)
 
 if [ "$sub_state" = "running" ]; then
