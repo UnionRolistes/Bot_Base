e011c783495c25a8d9750f325eb470c316de0374
Youssef <agoussalyo@eisti.eu>
2021-08-05 17:15:09 +0200

    Changed -e to -d in add repo script

--- add_repo.sh
+++ add_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script starts the discord bot of "L'Union des Rôlistes"
 # TODO add copyright to follow bash scripts rules
-tmp='tmp/ur_bot/cogs' # Where the repos are cloned. TODO : change to /usr/local/src
+tmp='/usr/local/src'            #'tmp/ur_bot/cogs' # Where the repos are cloned. TODO : change to /usr/local/src
 ur_bot_dir='/usr/local/bin/URbot'
 cogs_folder="$ur_bot_dir/bot/cogs"
 repo="$tmp/$1"
@@ -17,12 +17,12 @@ if [ -z ${1+x} ]; then
   exit
 fi
 
-if [ ! -e "$repo" ] ; then
+if [ ! -d "$repo" ] ; then
   # downloads the repo from github.com
   git clone "https://github.com/UnionRolistes/$1.git" "$repo" # TODO add several cogs at once
 
   # checks that the clone was successful
-  if [ ! -e "$repo" ]; then
+  if [ ! -d "$repo" ]; then
     echo Failure
     exit
   fi
@@ -34,7 +34,7 @@ fi
 
 # installs cogs (located in the src folder and starting with 'cog_')
 echo Installing cogs...
-if [ ! -e $cogs_folder ] ; then
+if [ ! -d $cogs_folder ] ; then
   mkdir $cogs_folder
 fi
 find "$src" -maxdepth 1 -name "cog_*" -exec cp -vra '{}' $cogs_folder \;
@@ -48,15 +48,15 @@ echo
 echo Installation complete.
 
 # installs www
-if [ -e "$src/www" ]; then
-  if [ ! -e "$www/$1" ]; then
+if [ -d "$src/www" ]; then
+  if [ ! -d "$www/$1" ]; then
     mkdir -v "$www/$1"
   fi
   cp -vra "$src/www/." "$www/$1"
 fi
 
 # installs virtualhosts
-#if [ -e "$src"/sites-available ]; then
+#if [ -d "$src"/sites-available ]; then
  # cp -vra "$src"/sites-available/. $a2/sites-available
  # for f in "$src"/sites-available/*.conf; do
 #    a2ensite "$(basename "$f")"
